9659c9c1a5aeaf12e9045d84cb44211b3544430d
chrisyshorshort <chritinasarahwu@gmail.com>
2018-02-22 17:24:07 -0800

    minimum requirements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,10 +5,9 @@ $(document).ready(function () {
     var wrongCounter = 0;
     var unanswerCounter = 0;
     var count = 0;
-    var time = 10; 
+    var time = 15;
     var asked = [];
-    var questionsAsked = 0;   
-    var match;
+    var questionsAsked = 0;
     var userSelected;
     var interval;
 
@@ -16,73 +15,81 @@ $(document).ready(function () {
     var questions = [
         {
             question: 'Which frog is known for drinking tea?',
-            options: ['Kermit','Pepe','Frogger','Mr. Toad'],
+            options: ['Kermit', 'Pepe', 'Frogger', 'Mr. Toad'],
             answer: 'Kermit',
             picture: 'assets/images/kermitDrinkMeme.png',
-            usedQ: false,
         },
         {
             question: 'What is the breed of the Doge?',
-            options: ['Shiba Inu','French Bulldog','Huskie','Golden Retriever'],
+            options: ['Shiba Inu', 'French Bulldog', 'Huskie', 'Golden Retriever'],
             answer: 'Shiba Inu',
             picture: 'assets/images/codedoge.jpg',
-            usedQ: false,
         },
         {
             question: 'What is the saying used for memes with an 8-bit sunglasses and a cigerete?',
-            options: ['Say No to Drugs','Thug Life','Later Haters','Future Too Bright'],
+            options: ['Say No to Drugs', 'Thug Life', 'Later Haters', 'Future Too Bright'],
             answer: 'Thug Life',
             picture: 'assets/images/Thug-Life-Cool-Glasses-PNG.png',
-            usedQ: false,
         },
         {
             question: 'What recent horror film remake sparked a flood of memes luring people into a sewer?',
-            options: ['It','Saw','Black Mirror','Insidious'],
+            options: ['It', 'Saw', 'Black Mirror', 'Insidious'],
             answer: 'It',
             picture: 'assets/images/itMeme.jpg',
-            usedQ: false,
         },
         {
             question: 'Nyan cat in a combination of cat and what other thing?',
-            options: ['Naan','Yam','Nylon','Pop Tart'],
+            options: ['Naan', 'Yam', 'Nylon', 'Pop Tart'],
             answer: 'Pop Tart',
             picture: 'assets/images/nyanCat.gif',
-            usedQ: false,
         },
         {
             question: "What is Willy Wonka's state in his memes?",
-            options: ['Livid','Sad','Condescending','Happy'],
+            options: ['Livid', 'Sad', 'Condescending', 'Happy'],
             answer: 'Condescending',
             picture: 'assets/images/willywonkameme.jpg',
-            usedQ: false,
         },
         {
             question: 'Where is the famous Troll face from?',
-            options: ['Rage Comic','Tumblr','Reddit','John Willkin Comics'],
+            options: ['Rage Comic', 'Tumblr', 'Reddit', 'John Willkin Comics'],
             answer: 'Rage Comic',
             picture: 'assets/images/trollface.jpg',
-            usedQ: false,
         },
         {
-            question: 'Where is the aint nobody got time for that meme originate from?',
-            options: ['Reddit','Comic Book','Television Show','TV Interview'],
+            question: "Where is the ain't nobody got time for that meme originate from?",
+            options: ['Reddit', 'Comic Book', 'Television Show', 'TV Interview'],
             answer: 'TV Interview',
             picture: 'assets/images/cold-weather-meme.jpg',
-            usedQ: false,
         },
         {
             question: 'Which illustration is not a version of Pepe the Frog?',
-            options: ['Feels Good Man','Angry Pepe','Sad Frog','Pepe Hungry'],
+            options: ['Feels Good Man', 'Angry Pepe', 'Sad Frog', 'Pepe Hungry'],
             answer: 'Pepe Hungry',
             picture: 'assets/images/pepe-frog-640x480.jpg',
-            usedQ: false,
         },
         {
             question: 'Which actor is featured in the roll safe meme?',
-            options: ['Kayode Ewumi','Kevin Hart','Larry the Cable Guy','Will Ferrel'],
+            options: ['Kayode Ewumi', 'Kevin Hart', 'Larry the Cable Guy', 'Will Ferrel'],
             answer: 'Kayode Ewumi',
             picture: 'assets/images/rollsafe.jpg',
-            usedQ: false,
+        },
+        {
+            question: "What is Grumpy cat's real name?",
+            options: ['Mr. Grump', 'Snowball', 'Tardar Sauce', 'Ron'],
+            answer: 'Tardar Sauce',
+            picture: 'assets/images/grumpyCat.jpg',
+        },
+        {
+            question: "Which Kardashian is famously found contemplating in bed?",
+            options: ['Kourtney Kardashian', 'Kris Kardashian', 'Kloé Kardashian', 'Kim Kardashian'],
+            answer: 'Kim Kardashian',
+            picture: 'assets/images/kimKMeme.jpg',
+        },
+        {
+            question: "Which rapper has a Jackie Chan meme tattoed?",
+            options: ['MadeinTYO', 'T-Pain', 'Big Sean', 'Mac Miller'],
+            answer: 'T-Pain',
+            picture: 'assets/images/tpain.png',
         },
     ]
 
@@ -90,16 +97,16 @@ $(document).ready(function () {
     function initGame() {
         $("#restartBTN").hide()
         $("#countdown").hide()
-        }
-    
+    }
+
     // Function to decrease the time by an interval of 1 second
     function timeCount() {
-        time= 10;
-        interval=setInterval(timeRemaining,1000);
+        time = 15;
+        interval = setInterval(timeRemaining, 1000);
     }
 
     // // What happens when you hit the Start Button //
-    $("#startBTN").on("click", function(){
+    $("#startBTN").on("click", function () {
         $("#startBTN").hide()
         $("#countdown").show()
         newQuestion()
@@ -108,16 +115,16 @@ $(document).ready(function () {
     //  Timer and Function to decrease the time in the timer
     function timeRemaining() {
         time--;
-        $("#countdown").html("Time Left : "+ time +" seconds")
-            // When the timer reaches 0
-            if (time == 0) {
-                clearInterval(interval);
-                UNcorrectAnswer()
-                setTimeout(function(){
-                    newQuestion();
-                },4000);
-            }
+        $("#countdown").html("Time Left : " + time + " seconds")
+        // When the timer reaches 0
+        if (time == 0) {
+            clearInterval(interval);
+            UNcorrectAnswer()
+            setTimeout(function () {
+                newQuestion();
+            }, 4000);
         }
+    }
 
     // Function of Bringing Up Questions, Options, and Answers
     function newQuestion() {
@@ -129,27 +136,24 @@ $(document).ready(function () {
         chosenAnswer = [$(randomQuestion).attr("answer")]
         chosenPicture = [$(randomQuestion).attr("picture")]
         $("#questionArea").html(chosenQuestion)
-        $(randomQuestion).attr('usedQ', true)
-        asked.push(randomQuestion)
-        questionsAsked ++
+        questionsAsked++
         $("#optionsArea").show()
-        console.log(asked)
-        console.log(questions)
         if (questionsAsked === 11) {
-            clearInterval(interval); 
-            endGame ()
+            clearInterval(interval);
+            endGame()
         }
         // For loop to append the different options as buttons
         for (var i = 0; i < 4; i++) {
-            $("#optionsArea").append("<br><button id='option' value='"+randomQuestion.options[i]+"'>" + randomQuestion.options[i] + "</button>")
+            $("#optionsArea").append("<br><button id='option' value='" + randomQuestion.options[i] + "'>" + randomQuestion.options[i] + "</button>")
         }
-    // }
-        // NEED else statement so that when value usedQ is true it will run function to choose need word
+        var removeItem = randomQuestion
+        questions.splice($.inArray(removeItem, questions), 1);
     }
 
+    // What happens when you click on an option
     $(document).on("click", "#option", function () {
         userSelected = $(this).attr("value")
-        if (userSelected == chosenAnswer){
+        if (userSelected == chosenAnswer) {
             correctAnswer()
         }
         else {
@@ -157,51 +161,51 @@ $(document).ready(function () {
         }
     });
 
-    // What happens when an answer is not selected
+    // What happens when an option is not selected
     function UNcorrectAnswer() {
-        unanswerCounter ++;
+        unanswerCounter++;
         var UNanswerDiv = $('<div class="UNcorrAnswer">');
-        UNcorrAnswr = UNanswerDiv.append("Time's up!<br><br>The correct answer is:<br>" + chosenAnswer + "<br><br>" + "<img src='" + chosenPicture + "'style='width:200px;height:200px;'/>")
+        UNcorrAnswr = UNanswerDiv.append("Time's up!<br><br>The correct answer is:<br><span id='answerCSS'>" + chosenAnswer + "</span><br><br>" + "<img src='" + chosenPicture + "'style='width:200px;height:200px;'/>")
         $("#questionArea").html(UNcorrAnswr);
         $("#optionsArea").empty()
-        setTimeout(function(){
+        setTimeout(function () {
             $('div').remove('.UNcorrAnswer');
-        },4000);
+        }, 4000);
     }
 
     // What happens when an incorrect answer is selected
     function wrongAnswer() {
-        wrongCounter ++;
+        wrongCounter++;
         var WRanswerDiv = $('<div class="WRcorrAnswer">');
-        WRanswr = WRanswerDiv.html("Wrong Choice!<br><br>The correct answer is:<br>" + chosenAnswer + "<br><br>" + "<img src='" + chosenPicture + "'style='width:200px;height:200px;'/>")
+        WRanswr = WRanswerDiv.html("Wrong Choice!<br><br>The correct answer is:<br><span id='answerCSS'>" + chosenAnswer + "</span><br><br>" + "<img src='" + chosenPicture + "'style='width:200px;height:200px;'/>")
         $("#questionArea").html(WRanswr);
         $("#optionsArea").empty()
-        clearInterval(interval); 
-        setTimeout(function(){
+        clearInterval(interval);
+        setTimeout(function () {
             $('div').remove('.WRcorrAnswer');
             newQuestion();
-        },4000);
+        }, 4000);
     }
 
     // What happens when the correct answer is selected
     function correctAnswer() {
-        correctCounter ++;
+        correctCounter++;
         var answerDiv = $('<div class="corrAnswer">');
         corrAnswr = answerDiv.append("Much correct, such awesome!<br><br>" + "<img src='" + chosenPicture + "'style='width:200px;height:200px;'/>")
         $("#questionArea").html(corrAnswr);
         $("#optionsArea").empty()
-        clearInterval(interval); 
-        setTimeout(function(){
+        clearInterval(interval);
+        setTimeout(function () {
             $('div').remove('.corrAnswer');
             newQuestion();
-        },4000);
+        }, 4000);
     }
 
     // Function to show what happens at the end of the game
-    function endGame () {
+    function endGame() {
         reset();
         var endDiv = $('<div class="ending">');
-        end = endDiv.html("End of the Road, your final results: <br>" + "<p>Correct Answers: " +correctCounter+"</p>" +
+        end = endDiv.html("End of the Road, your final results: <br>" + "<p>Correct Answers: " + correctCounter + "</p>" +
             "<p>Incorrect Answers: " + wrongCounter + "</p>" + "<p>Unanswered: " + unanswerCounter + "</p><img src='assets/images/grumpyGameOver.jpg' style='width:200px;height:200px;'/><button id='restartBTN' class='btn'>Restart</button><img src='assets/images/gameOver.jpg' style='width:200px;height:200px;'/>")
         $("#questionArea").html(end);
         $("#restartBTN").show()
@@ -220,7 +224,86 @@ $(document).ready(function () {
         wrongCounter = 0
         unanswerCounter = 0
         questionsAsked = 0
-        questions.usedQ = false
+        var questions = [
+            {
+                question: 'Which frog is known for drinking tea?',
+                options: ['Kermit', 'Pepe', 'Frogger', 'Mr. Toad'],
+                answer: 'Kermit',
+                picture: 'assets/images/kermitDrinkMeme.png',
+            },
+            {
+                question: 'What is the breed of the Doge?',
+                options: ['Shiba Inu', 'French Bulldog', 'Huskie', 'Golden Retriever'],
+                answer: 'Shiba Inu',
+                picture: 'assets/images/codedoge.jpg',
+            },
+            {
+                question: 'What is the saying used for memes with an 8-bit sunglasses and a cigerete?',
+                options: ['Say No to Drugs', 'Thug Life', 'Later Haters', 'Future Too Bright'],
+                answer: 'Thug Life',
+                picture: 'assets/images/Thug-Life-Cool-Glasses-PNG.png',
+            },
+            {
+                question: 'What recent horror film remake sparked a flood of memes luring people into a sewer?',
+                options: ['It', 'Saw', 'Black Mirror', 'Insidious'],
+                answer: 'It',
+                picture: 'assets/images/itMeme.jpg',
+            },
+            {
+                question: 'Nyan cat in a combination of cat and what other thing?',
+                options: ['Naan', 'Yam', 'Nylon', 'Pop Tart'],
+                answer: 'Pop Tart',
+                picture: 'assets/images/nyanCat.gif',
+            },
+            {
+                question: "What is Willy Wonka's state in his memes?",
+                options: ['Livid', 'Sad', 'Condescending', 'Happy'],
+                answer: 'Condescending',
+                picture: 'assets/images/willywonkameme.jpg',
+            },
+            {
+                question: 'Where is the famous Troll face from?',
+                options: ['Rage Comic', 'Tumblr', 'Reddit', 'John Willkin Comics'],
+                answer: 'Rage Comic',
+                picture: 'assets/images/trollface.jpg',
+            },
+            {
+                question: "Where is the ain't nobody got time for that meme originate from?",
+                options: ['Reddit', 'Comic Book', 'Television Show', 'TV Interview'],
+                answer: 'TV Interview',
+                picture: 'assets/images/cold-weather-meme.jpg',
+            },
+            {
+                question: 'Which illustration is not a version of Pepe the Frog?',
+                options: ['Feels Good Man', 'Angry Pepe', 'Sad Frog', 'Pepe Hungry'],
+                answer: 'Pepe Hungry',
+                picture: 'assets/images/pepe-frog-640x480.jpg',
+            },
+            {
+                question: 'Which actor is featured in the roll safe meme?',
+                options: ['Kayode Ewumi', 'Kevin Hart', 'Larry the Cable Guy', 'Will Ferrel'],
+                answer: 'Kayode Ewumi',
+                picture: 'assets/images/rollsafe.jpg',
+            },
+            {
+                question: "What is Grumpy cat's real name?",
+                options: ['Mr. Grump', 'Snowball', 'Tardar Sauce', 'Ron'],
+                answer: 'Tardar Sauce',
+                picture: 'assets/images/grumpyCat.jpg',
+            },
+            {
+                question: "Which Kardashian is famously found contemplating in bed?",
+                options: ['Kourtney Kardashian', 'Kris Kardashian', 'Kloé Kardashian', 'Kim Kardashian'],
+                answer: 'Kim Kardashian',
+                picture: 'assets/images/kimKMeme.jpg',
+            },
+            {
+                question: "Which rapper has a Jackie Chan meme tattoed?",
+                options: ['MadeinTYO', 'T-Pain', 'Big Sean', 'Mac Miller'],
+                answer: 'T-Pain',
+                picture: 'assets/images/tpain.png',
+            },
+        ]
     }
 
     // Restart Button to start game back to beginning
@@ -228,7 +311,7 @@ $(document).ready(function () {
         resetValue()
         reset()
         newQuestion()
-        });
+    });
 
     // // Calling the game to start //
     initGame()
